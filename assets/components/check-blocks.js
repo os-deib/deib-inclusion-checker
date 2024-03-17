868b84b926ff9f78f23537437992f433407b9068
Bernhard Kau <b.kau@inpsyde.com>
2024-03-17 20:48:29 +0100

    Show a spinner, while the check is loading

--- assets/components/check-blocks.js
+++ assets/components/check-blocks.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from '@wordpress/element';
-import { useSelect } from '@wordpress/data';
+import { Spinner } from '@wordpress/components';
 import { useDebounce } from '@wordpress/compose';
+import { useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 
 import apiFetch from '@wordpress/api-fetch';
@@ -10,6 +11,7 @@ const CheckBlocks = () => {
 	const blocks = useSelect( ( select ) => select( 'core/block-editor' ).getBlocks() );
 
 	const [ wordCheckerIssues, setWordCheckerIssues ] = useState( null );
+	const [ isLoading, setIsLoading ] = useState( false );
 
 	useEffect( () => {
 		updateBlocksWithDebounce( blocks );
@@ -20,14 +22,22 @@ const CheckBlocks = () => {
 	}, 500 );
 
 	const checkBlocks = () => {
+		setIsLoading( true );
+
 		apiFetch( {
 			data: { blocks },
 			method: 'POST',
 			path: '/deibic/v1/parseblocks',
 		} ).then( ( res ) => {
 			setWordCheckerIssues( res );
+			setIsLoading( false );
 		} );
 	};
+
+	if (isLoading) {
+		return <Spinner/>;
+	}
+
 	return (
 		<>
 			{
