1b240ed2f9712e654df972ccd12270db48936a98
Bernhard Kau <b.kau@inpsyde.com>
2024-03-17 18:00:16 +0100

    Remove early return when not in admin

--- deib-inclusion-checker.php
+++ deib-inclusion-checker.php
@@ -19,19 +19,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	return;
 }
 
-// we only need the following stuff in the admin area.
-if ( ! is_admin() ) {
-	return;
-}
-
-if ( ! function_exists( 'debug' ) ) {
-	function debug( $a, $b = false ) {
-		echo '<pre>';
-		! $b ? print_r( $a ) : var_dump( $a );
-		echo '</pre>';
-	}
-}
-
 // set needed constant.
 define( 'DEIBIC_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 
