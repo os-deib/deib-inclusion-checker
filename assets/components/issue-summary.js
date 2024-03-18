25a52234ede95749b1400a95ef916a2d5872347e
Laura Herzog <laura-herzog@outlook.com>
2024-03-18 09:49:10 +0100

    Polished CSS

--- assets/components/issue-summary.js
+++ assets/components/issue-summary.js
@@ -1,22 +1,22 @@
 import { Icon } from '@wordpress/components';
-import { info, plusCircle, help } from '@wordpress/icons';
+import { next, warning, info } from '@wordpress/icons';
 import { __ } from '@wordpress/i18n';
 
 const IssueSummary = ( { item } ) => {
 	return <details>
 		<summary>
 			<div className="issue-row">
-				<Icon icon={ info } className="icon issue"/>
+				<Icon icon={ warning } width="15" height="15" className="icon issue"/>
 				{ item.phrase }
 				<span className="toggle">{ __( "Why?", "deib-inclusion-checker" ) }</span>
 			</div>
 			<div className="alternatives-row">
-				<Icon icon={ plusCircle } className="icon alternatives"/>
+				<Icon icon={ next } width="15" height="15" className="icon alternatives"/>
 				{ item.alternatives.length ? item.alternatives.join( ', ' ) : `(${ item.suggested_action })` }
 			</div>
 		</summary>
 		<div className="explanation">
-			<Icon icon={ help } className="icon explanation"/>
+			<Icon icon={ info }  width="15" height="15" className="icon explanation"/>
 			{ item.explanation ?? __( "(word/phrase is missing an explanation)", "deib-inclusion-checker" ) }
 		</div>
 	</details>;
